053b570917d512361457ea16041dac487fd75d43
Manuel <santiagomanueldariocostilla@gmail.com>
2023-12-14 13:06:34 -0300

    cambios2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,34 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        'main': 'repeat(3, minmax(0, 2fr))',
-        },
-        gridTemplateRows: {
-          // Simple 8 row grid
-          'main-row': 'repeat(2, minmax(150px))',
-          },
+        main: 'repeat(3, minmax(0, 2fr))'
+      },
+      gridTemplateRows: {
+        // Simple 8 row grid
+        'main-row': 'repeat(2, minmax(150px))'
+      },
       colors: {
         'blue-header': '#0170B9',
         'gray-nav': '#E6E6E6',
         'green-nav': '#3EAF53',
         'black-footer': '#333333',
-        'emergencia': '#BDBFC1',
-        'policia': '#00AFEF',
-        'bombero': '#Ed3237',
-        'hospital': '#00A859',
-        'genero': '#A8518A',
+        emergencia: '#BDBFC1',
+        policia: '#00AFEF',
+        bombero: '#Ed3237',
+        hospital: '#00A859',
+        genero: '#A8518A'
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }

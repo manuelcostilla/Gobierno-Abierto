3ef2a8c3a5f2d333423c686ede859c80e6c2eaa5
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-07-07 19:33:59 -0300

    Se mejoro la version mobile, se agregaron breakpoints y tamaño de fuentes en talwind.config, se agrego nextUi para poder usar componentes.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import { Main } from '@/components/Main'
+import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider } from 'next-themes'
 export default function Home () {
   return (
     <>
+    <ThemeProvider>
+    <NextUIProvider>
       <Main />
+      </NextUIProvider>
+    </ThemeProvider>
     </>
   )
 }
--- app/presupuesto-publico/components/Main.jsx
+++ app/presupuesto-publico/components/Main.jsx
@@ -4,39 +4,39 @@ import React from 'react'
 const Main_presupuesto = () => {
   return (
     <>
-      <div className="flex h-36 w-full bg-blue-title justify-center items-center">
-        <h1 className="text-4xl font-sans text-white font-bold"style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}>PRESUPUESTO PUBLICO</h1>
+        <div className="flex h-36 w-full bg-blue-title justify-center items-center text-center">
+        <h1 className="text-4xl font-sans text-white font-bold" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}>PRESUPUESTO PUBLICO</h1>
       </div>
-      <div className="flex p-8 break-normal">
+      <div className="flex p-8 break-normal font-sans">
         <p>Información de gastos en personal, no personales, bienes de consumo, bienes de personal, y otros gastos, realizados por el Municipio de Baradero.</p>
       </div>
-      <div className="flex justify-evenly items-center m-3 mb-12">
-        <div className='border-solid border-2 border-blue-header rounded-lg p-10 mx-4'>
+      <div className="flex justify-evenly items-center m-3 mb-12 lg:m-3 font-sans flex-col lg:flex-row">
+        <div className='border-solid border-2 border-blue-header rounded-lg p-10 mx-4 lg:m-2 xs:m-2'>
+          <h1 className='text-blue-header font-bold text-xl p-2 mb-2'>Presupuesto Municipal</h1>
+          <p className='mb-6 '>Información en gastos de personal, no personales, bienes de consumo, bienes de personal y otros gastos, realizados por el municipio de Baradero</p>
+          <Link href="/presupuesto-publico/gastos">
+            <button className="mx-2 bg-blue-500 hover:bg-blue-700 text-white rounded-lg font-bold py-2 px-4 w-[150px]">
+              Gastos
+            </button>
+          </Link>
+        </div>
+        <div className='border-solid border-2 border-blue-header rounded-lg p-10 mx-4 lg:m-2 xs:m-2 font-sans'>
           <h1 className='text-blue-header font-bold text-xl p-2 mb-2'>Presupuesto Municipal</h1>
           <p className='mb-6'>Información en gastos de personal, no personales, bienes de consumo, bienes de personal y otros gastos, realizados por el municipio de Baradero</p>
-        <Link href="/presupuesto-publico/gastos">
-          <button className="mx-2 bg-blue-500 hover:bg-blue-700 text-white rounded-lg font-bold py-2 px-4 w-[150px]">
-           Gastos
-          </button>
-        </Link>
+          <Link href="/presupuesto-publico/ingresos">
+            <button className="mx-2 bg-blue-500 rounded-lg hover:bg-blue-700 text-white font-bold py-2 px-4 w-[150px]">
+              Ingresos
+            </button>
+          </Link>
         </div>
-        <div className='border-solid border-2 border-blue-header rounded-lg p-10 mx-4'>
-        <h1 className='text-blue-header font-bold text-xl p-2 mb-2'>Presupuesto Municipal</h1>
+        <div className='border-solid border-2 border-blue-header rounded-lg p-10 mx-4 lg:m-2 xs:m-2 font-sans'>
+          <h1 className='text-blue-header font-bold text-xl p-2 mb-2'>Presupuesto Municipal</h1>
           <p className='mb-6'>Información en gastos de personal, no personales, bienes de consumo, bienes de personal y otros gastos, realizados por el municipio de Baradero</p>
-        <Link href="/presupuesto-publico/ingresos">
-          <button className="mx-2 bg-blue-500 rounded-lg hover:bg-blue-700 text-white font-bold py-2 px-4 w-[150px]">
-            Ingresos
-          </button>
-        </Link>
-        </div>
-        <div className='border-solid border-2 border-blue-header rounded-lg p-10 mx-4'>
-        <h1 className='text-blue-header font-bold text-xl p-2 mb-2'>Presupuesto Municipal</h1>
-          <p className='mb-6'>Información en gastos de personal, no personales, bienes de consumo, bienes de personal y otros gastos, realizados por el municipio de Baradero</p>
-        <Link href="/presupuesto-publico/ingresos">
-          <button className="mx-2 bg-blue-500 rounded-lg hover:bg-blue-700 text-white font-bold py-2 px-4 w-[150px]">
-            Presupuesto
-          </button>
-        </Link>
+          <Link href="/presupuesto-publico/ingresos">
+            <button className="mx-2 bg-blue-500 rounded-lg hover:bg-blue-700 text-white font-bold py-2 px-4 w-[150px]">
+              Presupuesto
+            </button>
+          </Link>
         </div>
       </div>
     </>

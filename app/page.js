791683250f91161b25b31c594fd3790bfdc7096e
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-08-01 12:42:46 -0300

    Cambios + agregando Shadcdn y quitando NextUI

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
 import { Main } from '@/components/Main'
-import { NextUIProvider } from '@nextui-org/react'
-import { ThemeProvider } from 'next-themes'
 
 export default function Home () {
   return (
     <>
-    <ThemeProvider>
-    <NextUIProvider>
       <Main />
-      </NextUIProvider>
-    </ThemeProvider>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-import { nextui } from '@nextui-org/react'
-
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx,mdx}'
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
@@ -45,21 +45,9 @@ export default {
         '8xl': '6rem',
         '9xl': '8rem',
         'custom-size': '2.5rem' // Tamaño personalizado
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       }
+
     },
-    screens: {
-      xs: '390px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px'
-    }
-  },
-  darkMode: 'class',
-  plugins: [nextui()]
+    plugins: []
+  }
 }

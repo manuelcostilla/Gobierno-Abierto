63586caadaf34e7692dc593403f443a0d48b1732
Manuel <santiagomanueldariocostilla@gmail.com>
2024-08-01 19:52:50 -0300

    borrando tailwind.config.js y dejando solo .ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   prefix: "",
   theme: {
     container: {
@@ -52,6 +52,18 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        'blue-header': '#0170b9',
+        'gray-nav': '#E6E6E6',
+        'green-nav': '#3eaf53',
+        'black-footer': '#333333',
+        'black-nav': '#909092',
+        'blue-btn': '#4a8eff',
+        'blue-title': '#00689c',
+        emergencia: '#BDBFC1',
+        policia: '#00AFEF',
+        bombero: '#Ed3237',
+        hospital: '#00A859',
+        genero: '#A8518A'
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -72,9 +84,33 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-    },
+      gridTemplateColumns: {
+        main: 'repeat(3, minmax(0, 2fr))'
+      },
+      gridTemplateRows: {
+        'main-row': 'repeat(2, minmax(150px))'
+      },
+      fontSize: {
+        xs: '.75rem',
+        sm: '.8125rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+        'custom-size': '2.5rem' // Tamaño personalizado
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [
+    require("tailwindcss-animate"),
+  ]
+};
 
-export default config
+export default config;

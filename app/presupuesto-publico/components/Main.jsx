5d60b8416ed9afa878ddf35796448232d79ff418
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-08-01 13:02:19 -0300

    ultimo buttonn

--- app/presupuesto-publico/components/Main.jsx
+++ app/presupuesto-publico/components/Main.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { Button } from '@nextui-org/react'
+import { Button } from '@/components/ui/button'
 const Main_presupuesto = () => {
   return (
     <>

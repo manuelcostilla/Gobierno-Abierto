1b3263e03d786dccf4126af1958839e6f9b24ec8
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-08-01 12:42:46 -0300

    Cambios + agregando Shadcdn y quitando NextUI

--- app/Gestion-de-compromisos/components/CompromisosCard.jsx
+++ app/Gestion-de-compromisos/components/CompromisosCard.jsx
@@ -1,28 +1,42 @@
 import React from 'react'
-import { Card, CardHeader, CardBody, Image, Button } from '@nextui-org/react'
 import { dataObra } from '../dataObra/dataObraJSON'
+import { Progress } from '@/components/ui/progress'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 const CompromisosCard = () => {
   return (
-    <div className='flex flex-wrap justify-center m-20 '>
+    <div className="flex flex-wrap justify-center m-20 ">
       {dataObra.map((data, index) => (
         <Card key={index} className="relative py-4 flex m-5 h-80 w-60">
-          <div className="absolute inset-0 z-0">
-            <Image
-              alt="Card background"
-              className="object-cover w-full h-80 rounded-xl"
-              src={data.image}
-            />
-          </div>
           <div className="relative z-10 flex flex-col items-center p-4 bg-white bg-opacity-25 h-full justify-center rounded-xl">
-            <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
-              <p className="text-tiny uppercase font-bold">{data.title}</p>
-              <Button color="primary" className='text-white bg-blue-header h-11 mt-2'>Ver mas</Button>
-            </CardHeader>
-            <CardBody className="overflow-visible py-2 flex flex-col items-center w-full">
+            <div className="pb-0 pt-2 px-4 flex-col items-center">
+              <CardHeader>
+                <CardTitle className="text-tiny">
+                  {data.title}
+                </CardTitle>
+              </CardHeader>
+            </div>
+             <CardContent className="absolute">
+             {data.description}
+          </CardContent>
+            <div className="overflow-visible py-2 flex flex-col items-center w-full">
               {/* Puedes eliminar esta sección si no necesitas el CardBody */}
-              <h4 className='text-white'>Progreso de obra</h4>
-            </CardBody>
+              <Progress value={33} className="w-[60%] bg-slate-700" />
+            </div>
+            <Button
+              color="primary"
+              className="text-white bg-blue-header h-11 mt-2"
+            >
+              Ver mas
+            </Button>
           </div>
         </Card>
       ))}

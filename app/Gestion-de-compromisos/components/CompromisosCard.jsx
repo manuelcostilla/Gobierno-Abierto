b059c3dfc3f4f8cfcbd93d979e5434919889b28a
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-08-02 08:56:12 -0300

    Avances Gestion de compromisos

--- app/Gestion-de-compromisos/components/CompromisosCard.jsx
+++ app/Gestion-de-compromisos/components/CompromisosCard.jsx
@@ -1,45 +1,74 @@
-import React from 'react'
-import { dataObra } from '../dataObra/dataObraJSON'
-import { Progress } from '@/components/ui/progress'
-import { Button } from '@/components/ui/button'
+import React from "react"
+import { dataObra } from "../data/dataObraJSON"
+import { Progress } from "@/components/ui/progress"
+import { Button } from "@/components/ui/button"
+// import Image from "next/image"
 import {
   Card,
   CardContent,
   CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 const CompromisosCard = () => {
   return (
-    <div className="flex flex-wrap justify-center m-20 ">
-      {dataObra.map((data, index) => (
-        <Card key={index} className="relative py-4 flex m-5 h-80 w-60 justify-center self-center">
-          <div className="relative z-10 flex flex-col items-center p-4 bg-white bg-opacity-25 h-full justify-center rounded-xl">
-            <div className="pb-0 pt-2 px-4 flex-col items-center">
-              <CardHeader>
-                <CardTitle className="text-xl">
-                  {data.title}
-                </CardTitle>
-              </CardHeader>
-            </div>
-             <CardContent className="absolute">
-             {data.description}
-          </CardContent>
-            <div className="overflow-visible py-2 flex flex-col items-center w-full">
-              {/* Puedes eliminar esta sección si no necesitas el CardBody */}
-              <Progress value={33} className="w-[60%] bg-slate-700" />
-            </div>
-            <Button
-              color="primary"
-              className="text-white bg-blue-header h-11 mt-2"
-            >
-              Ver mas
-            </Button>
-          </div>
-        </Card>
-      ))}
+    <div className="flex justify-center items-center">
+      <div className="flex flex-wrap justify-center self-center m-20 w-[1250px]">
+        {dataObra.map((data, index) => (
+          <Card key={index} className="w-[250px] m-5">
+            <CardHeader className="flex items-center justify-center bg-blue-header">
+              <CardTitle className="text-xl">{data.title}</CardTitle>
+            </CardHeader>
+            <CardContent className="text-xs text-wrap w-[250px] h-[150px] flex justify-center items-center border-b  mt-2">
+              <div>{data.description}</div>
+            </CardContent>
+            <CardContent>
+              <div className="mt-4">
+                <Progress value={data.progressvalue} className="bg-white" />
+                <Label>{data.progressvalue}%</Label>
+              </div>
+            </CardContent>
+            <CardFooter className="flex items-center justify-center">
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button
+                    color="primary"
+                    className="text-white bg-blue-header h-10 mt-2 rounded-[25px] hover:bg-blue-400"
+                  >
+                    Ver mas
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>{data.title}</DialogTitle>
+                  </DialogHeader>
+                  <DialogDescription>{data.description}</DialogDescription>
+                  <DialogFooter>
+                    {/* error no renderiza la imagen */}
+                    {/* <Image
+                      alt="Imagen obra"
+                      height={150}
+                      width={150}
+                      src="/./trabajador.png"
+                    /> */}
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }

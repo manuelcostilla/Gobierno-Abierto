cb279fd0e332ebeb271ad1d1168d57b0042edf25
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-08-02 08:56:12 -0300

    Avances Gestion de compromisos

---
+++ app/Gestion-de-compromisos/data/dataObraJSON.js
@@ -0,0 +1,74 @@
+export const dataObra = [
+  {
+    id: 1,
+    title: "Construcción acceso",
+    description:
+      "El proyecto consiste en la construcción de una carretera de acceso a la ciudad de Puno, que incluye la construcción de un puente sobre el río Puno y la construcción de una carretera de acceso a la ciudad de Puno.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 30,
+  },
+  {
+    id: 2,
+    title: "Construcción",
+    description:
+      "El proyecto consiste en la construcción de un hospital en la ciudad de Cusco, que incluye la construcción de un hospital de 200 camas y la construcción de un hospital de 200 camas.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 10,
+  },
+  {
+    id: 3,
+    title: "Construcción",
+    description:
+      "El proyecto consiste en la construcción de un puente en la ciudad de Arequipa, que incluye la construcción de un puente sobre el río Arequipa y la construcción de un puente sobre el río Arequipa.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 40,
+  },
+  {
+    id: 4,
+    title: "Construcción",
+    description:
+      "El proyecto consiste en la construcción de un hospital en la ciudad de Lima, que incluye la construcción de un hospital de 200 camas y la construcción de un hospital de 200 camas.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 60,
+  },
+  {
+    id: 5,
+    title: "Construcción",
+    description:
+      "El proyecto consiste en la construcción de un puente en la ciudad de Trujillo, que incluye la construcción de un puente sobre el río Trujillo y la construcción de un puente sobre el río Trujillo.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 80,
+  },
+  {
+    id: 6,
+    title: "Construcción",
+    description:
+      "El proyecto consiste en la construcción de un hospital en la ciudad de Piura, que incluye la construcción de un hospital de 200 camas y la construcción de un hospital de 200 camas.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 100,
+  },
+  {
+    id: 7,
+    title: "Construcción",
+    description:
+      "El proyecto consiste en la construcción de un puente en la ciudad de Iquitos, que incluye la construcción de un puente sobre el río Iquitos y la construcción de un puente sobre el río Iquitos.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 45,
+  },
+  {
+    id: 8,
+    title: "Construcción",
+    description:
+      "El proyecto consiste en la construcción de un hospital en la ciudad de Chiclayo, que incluye la construcción de un hospital de 200 camas y la construcción de un hospital de 200 camas.",
+    image:
+      "https://baradero.gob.ar/wp-content/uploads/2023/11/Obra-finalizada-costanera-sur.jpg",
+    progressvalue: 15,
+  },
+]

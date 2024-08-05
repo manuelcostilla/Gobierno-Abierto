00a8e005ea9930a0f815ff0083d2b3cb05067381
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-08-05 10:11:51 -0300

    abc

--- app/organigrama/components/Organigrama.jsx
+++ app/organigrama/components/Organigrama.jsx
@@ -2,40 +2,30 @@ import { Button } from "@/components/ui/button"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import Image from "next/image"
 export function Organigrama () {
   return (
     <>
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[925px] sm:max-h-[600px] bg-slate-200">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make.
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-    <div className="grid gap-4 py-4 sm:max-w-[745px] sm:max-h-[593px] ">
-    <div className="w-full h-full">
-      <Image alt="Imagen obra"
-                height={740}
-                width={593}
-                src="/imagenes/org.png" className="w-full h-full object-cover rounded-sm"/>
-    </div>
-  </div>
-  </>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className=" max-h-[1240px] max-w-[1240px]">
+          <div className="w-full h-full">
+            <Image
+              alt="Imagen obra"
+              height={1240}
+              width={1240}
+              src="/imagenes/Untitled-2024-08-05-1004.png"
+              className="w-full h-full object-cover rounded-sm"
+            />
+          </div>
+        </DialogContent>
+
+      </Dialog>
+      <div className="grid gap-4 py-4 sm:max-w-[745px]  "></div>
+    </>
   )
 }

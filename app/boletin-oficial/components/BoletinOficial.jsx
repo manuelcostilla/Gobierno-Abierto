374fac62ccfadf20d48a990ae5838500d0b62ce6
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-08-07 11:38:35 -0300

    Update BoletinOficial component styling and layout

--- app/boletin-oficial/components/BoletinOficial.jsx
+++ app/boletin-oficial/components/BoletinOficial.jsx
@@ -7,8 +7,8 @@ export const BoletinOficial = ({ data }) => {
       {data.map((data, index) => (
         <div key={index} className="flex w-full lg:w-[1100px] mb-10 m-auto p-10 border-dotted border border-gray-400 clip-your-needful-style text-base xs:text-xs">
           <div className="w-full">
-            <div className="lg:flex lg:flex-col">
-                <div className="mb-4 flex ">
+            <div className="lg:flex justify-between items-center">
+                <div className=" flex ">
                   <p className="text-center lg:text-left">{data.texto}</p>
                 </div>
                 <Button asChild className='bg-blue-header hover:bg-blue-500'>

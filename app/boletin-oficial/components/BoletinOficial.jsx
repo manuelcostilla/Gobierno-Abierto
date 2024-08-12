6d363bbc14eb728f28ba7dd766c660f31f5e80e9
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-08-12 11:05:28 -0300

    Update BoletinOficial component styling and layout

--- app/boletin-oficial/components/BoletinOficial.jsx
+++ app/boletin-oficial/components/BoletinOficial.jsx
@@ -1,11 +1,13 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-
+import { ScrollArea } from '@/components/ui/scroll-area'
 export const BoletinOficial = ({ data }) => {
   return (
       <>
+      <div className='flex justify-center'>
+      <ScrollArea className='flex justify-center w-full lg:w-[1050px] h-[400px] mb-20 border-dotted border border-gray-400'>
       {data.map((data, index) => (
-        <div key={index} className="flex w-full lg:w-[700px] lg:h-[80px] mb-10 m-auto font-sans border-dotted border border-gray-400 clip-your-needful-style text-base xs:text-xs">
+        <div key={index} className="flex w-full lg:w-[700px] lg:h-[80px] mt-10 mb-10 m-auto font-sans border-dotted border border-gray-400 clip-your-needful-style text-base xs:text-xs">
           <div className="w-full flex items-center justify-between p-5">
                 <div className=" flex font-bold">
                   <p className="text-center lg:text-left">{data.texto}</p>
@@ -18,6 +20,7 @@ export const BoletinOficial = ({ data }) => {
           </div>
         </div>
       ))}
+      </ScrollArea></div>
       </>
   )
 }

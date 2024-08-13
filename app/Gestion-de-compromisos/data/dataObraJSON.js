889ffd64dfa475b43a761ab21dc78d160b8387f8
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-08-13 10:45:22 -0300

    Carousel

--- app/Gestion-de-compromisos/data/dataObraJSON.js
+++ app/Gestion-de-compromisos/data/dataObraJSON.js
@@ -5,7 +5,7 @@ export const dataObra = [
     description:
       "La primera etapa de la obra consistió en la recuperación del espacio del proyecto del Club de Yates que quedó sin efecto. En primera instancia de modificó la traza original del camino costero para retomar el camino original. Después se construyeron cordones, rotondas, entre otros trabajos. Luego, inició la segunda etapa de la obra en la zona del Circuito de Baradero. Allí se llevaron adelante trabajos de preparación de la superficie y nivelación de suelo para proceder y culminar con la colocación de la carpeta asfáltica.",
     progressvalue: 100,
-    Imagelocation: "/imagenes/costanero_sur_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Finalizó la pavimentación de más de 4 kilómetros de la Costanera Sur, obra que pone en valor la costa y que potencia al turismo. ",
   },
@@ -15,7 +15,7 @@ export const dataObra = [
     description:
       "Culminó la etapa de instalación de cañerías y comenzaron a realizarse tareas de empalme de agua en la cañería principal. El recambio de las redes de distribución de la zona del centro se desarrolla en el perímetro conformado por las calles Mariano Moreno, Darragueira, Anchorena y Paso. La misma se realiza a través de un sistema de sustitución-renovación de cañerías que no necesita del zanjeo, y ofrece beneficios como el acortamiento de los tiempos de ejecución, mínimo movimiento de suelos e intervención en las veredas, entre otros. Es llevada adelante por ENOHSA a través de la Secretaría de Obras y Servicios Públicos del Municipio de Baradero.",
     progressvalue: 87.25,
-    Imagelocation: "/imagenes/cañerias del centro.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "La obra histórica que cuenta con una inversión de $427.000.707 millones de pesos mejorará el servicio de agua en la zona del centro de Baradero.",
   },
@@ -25,7 +25,7 @@ export const dataObra = [
     description:
       "Mediante el proyecto Mercado Raíz, el Ministerio de Turismo y Deportes de la Nación invierte 25 millones de pesos para poner en valor el Paseo del Puerto. Ya se finalizó con el entubamiento del zanjón de calle Fiori, la colocación de nuevas luminarias y también con la construcción de senderos. Se está trabajando en la restauración de los galpones con tareas de pintura, recambio de chapas, construcción de nuevos baños, colocación de canaletas y nuevas bajantes, armado de escenario, del cartel señalético principal y de puestos de feriantes. Con esta obra, se podrán ampliar las propuestas turísticas, productivas y culturales, ofreciendo un espacio único que también te permite apreciar la naturaleza de la ciudad, ya que está próxima al río.",
     progressvalue: 83.11,
-    Imagelocation: "/imagenes/puesta_en_valor_puerto.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Sigue ejecutándose la obra de la puesta en valor del Paseo del Puerto a través del programa nacional “50 Destinos” ",
   },
@@ -35,7 +35,7 @@ export const dataObra = [
     description:
       "Se dio por terminada la segunda mano de calle Roca, donde se concretó la pavimentación del último tramo restante. Esta segunda etapa de la obra de mejoramiento de la red vial urbana, en la que se invirtió $64.493.745, contempló la construcción de cordones cunetas y entubamientos, la colocación de brea y por último las tareas de pavimentación de la calle que desemboca a la Ruta N°41.",
     progressvalue: 79.11,
-    Imagelocation: "/imagenes/pavimentacion_calle_roca_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Se dio por terminada la segunda mano de calle Roca, donde se concretó la pavimentación del último tramo restante.",
   },
@@ -45,7 +45,7 @@ export const dataObra = [
     description:
       "Inauguración de la primera Casa de la Provincia en territorio bonaerense, un espacio destinado a unificar y facilitar el acceso a todas las prestaciones que brinda el Estado. La construcción se llevó adelante con obra de mano local y materiales provenientes de comercios del municipio, contribuyendo además a la reactivación de la actividad y la generación de empleo. “Además de tener un sentido especial por ser la primera, esta Casa de la Provincia nos ha permitido generar trabajo genuino en Baradero y poner en valor al barrio”, sostuvo el intendente Sanzio. Con una inversión de $141 millones, la Casa de la Provincia se construyó en un terreno cedido por el municipio. Allí funcionarán las oficinas de ARBA, IOMA, el Registro Provincial de las Personas y el Instituto de Previsión Social (IPS); y las delegaciones de los ministerios de Trabajo y de las Mujeres, Políticas de Género y Diversidad Sexual. Durante la jornada, se puso en funcionamiento también una ambulancia de alta complejidad para fortalecer la gestión de emergencias en el Municipio de Baradero.",
     progressvalue: 100,
-    Imagelocation: "/imagenes/portada3_inaguracion Casa de la Provincia.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Inauguración de la primera Casa de la Provincia en territorio bonaerense, un espacio destinado a unificar y facilitar el acceso a todas las prestaciones que brinda el Estado.",
   },
@@ -55,7 +55,7 @@ export const dataObra = [
     description:
       "Fue un acontecimiento importante para la localidad baraderense por los 22 kilómetros pavimentados y la puesta en valor que favorece la conectividad, la producción, el turismo, la educación y la calidad de vida de todas y todos los vecinos porteleros. Para esta obra de repavimentación de la Ruta N°38, camino a Ireneo Portela el Estado destinó $329.964.187,54 de pesos.",
     progressvalue: 100,
-    Imagelocation: "/imagenes/camino_a_portela_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "La inauguración de la histórica obra estuvo encabezada por el intendente Esteban Sanzio y el gobernador de la provincia Axel Kicillof.",
   },
@@ -65,7 +65,7 @@ export const dataObra = [
     description:
       "Se trata del convenio firmado con el ministro de Obras Públicas de la Nación, Gabriel Katopodis, en su visita a Baradero en 2020 para la ejecución de obras por medio del Plan “Argentina Hace II”. En este caso, es una inversión de más de 25 millones de pesos destinados al mejoramiento del tránsito y desagüe de la zona. Específicamente son $25.187.840, 91 que contemplan 10.530 m2 de pavimento asfáltico, 1.056 metros lineales de cordón cuneta y 294 m2 de boca calles y badenes financiados en su totalidad por el Ministerio de Obras Públicas de la Nación.",
     progressvalue: 100,
-    Imagelocation: "/imagenes/anfiteatro_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "La obra consta de la construcción de la cinta asfáltica, entubamiento pluvial y construcción de cunetas badenes en el barrio.",
   },
@@ -75,7 +75,7 @@ export const dataObra = [
     description:
       "El Honorable Concejo Deliberante -autorizado por la Ordenanza 7180/23- aprobó la adjudicación de la obra de gas y calefacción para la Escuela Técnica N°1 que estará a cargo de la empresa Striking y contará con una inversión de $8.332.000 millones de pesos. Esta obra se suma a la obra integral del taller que ya culminó y constó de la reconstrucción del techo, los baños e incluyó tareas de adaptación de seguridad e higiene. También se llevaron adelante obras de instalación eléctricas que contemplaron la colocación de dos tableros principales, la instalación del nuevo tendido eléctrico por bandejas, bajadas de corriente nuevas para cada máquina e instalación eléctrica con cortina automatizada.",
     progressvalue: 75,
-    Imagelocation: "/imagenes/escuela_industria_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Siguen las obras en la Escuela Técnica N°1. Comienza la nueva obra de gas y calefacción.",
   },
@@ -85,7 +85,7 @@ export const dataObra = [
     description:
       "Los trabajos contemplan una inversión de más de 76.814.130,86 millones de pesos, contempla la reconstrucción del acceso principal de Alsina desde la Ruta Nacional N°9. La obra es llevada adelante por la Dirección Nacional de Vialidad del Ministerio de Obras Públicas de la Nación a través del programa de sostenimiento vial “Redes Conexas”.",
     progressvalue: 75,
-    Imagelocation: "/imagenes/acceso_alsina_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Esta obra de reconstrucción traerá mejoras a la conectividad y generará un acceso seguro para cada vecino.",
   },
@@ -95,7 +95,7 @@ export const dataObra = [
     description:
       "Las obras del Polideportivo Municipal “Miguel Ángel Mori” contempló la construcción de baños y vestuarios del Gimnasio Municipal, la nivelación del piso y la colocación de luminarias en pista y cancha. Además de las tareas desarrolladas en el sector del Gimnasio Municipal, en el Polideportivo se reacondicionó con la colocación polvo ladrillo, con una inversión de más de 150 mil pesos, la colocación de 31 luminarias y pintura de sus columnas y cordones de la pista; el acondicionamiento de la caja de salto y zona de lanzamientos, la refacción del alambrado periférico y la reparación de las columnas del SUM. La obra se llevó a cabo en tres etapas y la inversión fue de 12 millones de pesos. Esta obra permitió que Baradero pueda ser sede de importantes competencias en los distintos deportes que se practican.",
     progressvalue: 100,
-    Imagelocation: "/imagenes/polideportivo_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "A través del Programa Clubes en Obras se llevó adelante la obra del Polideportivo Municipal “Miguel Ángel Mori”.",
   },
@@ -105,7 +105,7 @@ export const dataObra = [
     description:
       "La obra contó con una inversión de más de 50 millones y constó de la ampliación del Espacio de Primera Infancia “Sonrisas”, que funciona dentro de las instalaciones del hogar, y de la remodelación de la cocina, los baños y las habitaciones de las niñas. Además de la construcción del SUM, la oficina del gabinete, las salas y las habitaciones de los niños. Luego de que finalice la primera etapa de la obra con tareas de ampliación y remodelación, se trabajo en la segunda etapa, que constó en la construcción del SUM, la oficina del gabinete, las salas y las habitaciones de los niños. Estas obras son de gran importancia para el desarrollo integral de los niños, niñas y jóvenes que allí residen, y para las y los trabajadores que acompañan cada día.",
     progressvalue: 100,
-    Imagelocation: "/imagenes/Hogar.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Finalizó la obra de ampliación y remodelación del Hogar del Niño “Juan XXIII” y de su Espacio de Primera Infancia “Sonrisas”, que fue promulgada y puesta en marcha gracias a las gestiones realizadas por el intendente Esteban Sanzio con el Ministerio de Desarrollo Social de la Nación. ",
   },
@@ -115,7 +115,7 @@ export const dataObra = [
     description:
       "Finalizó la obra de cordón cuneta y pavimentación en el barrio FONAVI que puso en valor la zona y mejoró la calidad de vida de cada familia que allí reside. La obra en ejecución tuvo una inversión de 34.890.802,48 millones de pesos y comprende las calles Gallo entre Sánchez Loria y René Simón; Medrano entre Castro Barros y José María Paz; y Castro Barros entre Gallo y Medrano.",
     progressvalue: 100,
-    Imagelocation: "/imagenes/pavimentacion_fonavi_obra.jpg",
+    Imagelocation: ["costanero_sur_obra.jpg", "puesta_en_valor_puerto.jpg", "pavimentacion_calle_roca_obra.jpg"],
     subtitle:
       "Finalizó la obra de cordón cuneta y pavimentación en el barrio FONAVI",
   },

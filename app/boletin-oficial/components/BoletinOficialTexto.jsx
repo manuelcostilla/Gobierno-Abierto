2906bbefb928ab62179c4beab8f0830577d16d0d
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-08-14 09:22:04 -0300

    Update BoletinOficialButtons styling and layout

--- app/boletin-oficial/components/BoletinOficialTexto.jsx
+++ app/boletin-oficial/components/BoletinOficialTexto.jsx
@@ -7,12 +7,10 @@ export const BoletinOficialTexto = () => {
     <h1 className='text-lg lg:text-3xl font-sans text-white font-bold 'style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}>BOLETIN OFICIAL</h1>
   </div>
   <div className='px-4 md:px-8 xs:flex xs:justify-center'>
-    <div className='flex justify-center'>
-      <h2 className='text-sm lg:text-lg w-full md:w-2/4 my-8 ml-20 md:my-20 text-slate-700 text-center md:text-left font-sans xs:w-full xs:ml-0'>
-        En esta página encontrarás links para consultar todos los boletines
-        oficiales de la Municipalidad de Baradero. Haz clic en el que te
-        interese y podrás verlo completo en PDF en pantalla. Lo puedes imprimir
-        o guardar en tu dispositivo.
+    <div className='flex justify-center text-balance'>
+      <h2 className='text-sm lg:text-lg w-full md:w-2/4 my-8 lg:ml-20 md:my-20 text-slate-700 text-center md:text-left font-sans xs:w-full'>
+        En esta sección encontrarás los Boletines
+        Oficiales del Municipio de Baradero. Puede consultar libre y públicamente el Boletín Oficial que le interese, verlo completo en PDF en pantalla, imprimir o guardar en su dispositivo.
       </h2>
     </div>
   </div>

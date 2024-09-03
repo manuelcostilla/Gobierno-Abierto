5b69901656d63a5b7590e1022dde850d9ea43a2f
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-09-03 13:17:28 -0300

    Add shortcuts component for quick access to different sections
    This commit adds a new component called "Atajos" that displays a set of shortcuts for quick access to different sections of the application. The shortcuts are rendered as cards with an image, title, and description. The data for the shortcuts is fetched from the "dataAtajos" module.
    
    The "Atajos" component is imported and used in the "Main" component to display the shortcuts on the main page.
    
    The commit also includes the addition of SVG images for the shortcuts, which are stored in the "public/atajos" directory.
    
    This feature will improve the user experience by providing easy navigation to frequently accessed sections of the application.

---
+++ components/dataAtajos.js
@@ -0,0 +1,42 @@
+export const dataAtajos = [
+  {
+    id: 1,
+    title: 'MAPA DEL ESTADO MUNICIPAL',
+    img: 'mapa_estado_municipal',
+    alt: 'Mapa del estado municipal',
+    descripcion: 'Es un diagrama que muestra la estructura jerárquica de funcional del gobierno local, indicando las áreas y sus relaciones. Es un aporte a la transparecia y un acercamiento a la ciudadanía a un gobierno abierto ya que permite conocer la estructura organizativa.',
+    link: '../organigrama',
+  },
+  {
+    id: 2,
+    title: 'BOLETÍN OFICIAL',
+    img: 'boletin_oficial',
+    alt: 'Boletín oficial',
+    descripcion: 'En esta sección encontrarás las publicaciones oficiales del municipio, como ordenanzas, decretos y resoluciones. Aqui se difuncen las decisiones y acciones importantes del gobierno local para mantenerte informado y garantizar la trasnparencia.',
+    link: '../BoletinOficial',
+  },
+  {
+    id: 3,
+    title: 'COMPRAS Y LICITACIONES',
+    img: 'compra_licitaciones',
+    alt: 'Compras y licitaciones',
+    descripcion: 'Aquí encontrarás detalles sobre las licitaciones y contratos del municipio. Incluye anuncios de licitaciones, contratos adjudicados, y detalles sobre proveedores y gastos. Esta sección asegura la transparencia al mostrar cómo se adjudican los contratos y se manejan las compras públicas.',
+    link: '../ComprasLicitac',
+  },
+  {
+    id: 4,
+    title: 'GESTIÓN DE COMPROMISOS',
+    img: 'gestion_compromisos',
+    alt: 'Gestión de compromisos',
+    descripcion: 'En esta sección, se detallan los compromisos del municipio relacionados con obras públicas, incluyendo proyectos en curso y futuros. Los ciudadanos pueden seguir el avance de las obras, conocer los plazos y presupuestos, y verificar el cumplimiento de los compromisos, promoviendo la transparencia y el control sobre el uso de los recursos públicos.',
+    link: '../GestionComprom',
+  },
+  {
+    id: 5,
+    title: 'PRESUPUESTO PUBLICO',
+    img: 'presupuesto_publico',
+    alt: 'Presupuesto público',
+    descripcion: 'Esta sección ofrece detalles sobre ingresos y gastos del municipio, incluyendo documentos, gráficos y reportes para asegurar transparencia y rendición de cuentas.',
+    link: '../PresupPublico',
+  }
+]

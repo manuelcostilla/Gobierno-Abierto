d073e27451b8286c864aba345ccb93f949435c77
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-09-04 12:25:58 -0300

    colores grafico

--- app/presupuesto-publico/components/Grafico_ingresos.tsx
+++ app/presupuesto-publico/components/Grafico_ingresos.tsx
@@ -1,14 +1,6 @@
 "use client"
 
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  XAxis,
-  LabelList,
-  Pie,
-  PieChart,
-} from "recharts"
+import { Bar, BarChart, CartesianGrid, XAxis, Pie, PieChart } from "recharts"
 
 import {
   ChartConfig,
@@ -19,54 +11,72 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart"
 
+import * as React from "react"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 const chartData = [
   {
     month: "Total Coparticipacion de impuestos",
-    coparticipacionimpuestos: 2049952360.0,
+    valor: 2049952360.0,
+    fill: "var(--color-coparticipacionimpuestos)"
   },
-  { month: "Total Tasas", tasas: 1221567000.0 },
-  { month: "Total Derechos", derechos: 145010000.0 },
-  { month: "Total Alquileres", alquileres: 3840000.0 },
-  { month: "Total Multas", multas: 62550000.0 },
-  { month: "Total Ingresos No Tributarios", notributarios: 1581445400.0 },
-  { month: "Total Intereses por prestamos", interesprestamos: 2000000.0 },
-  { month: "Total De administracion nacional", admnacional: 494741419.7 },
+  { month: "Total Tasas", valor: 1221567000.0, fill: "var(--color-tasas)" },
+  { month: "Total Derechos", valor: 145010000.0, fill: "var(--color-derechos)" },
+  { month: "Total Alquileres", valor: 3840000.0, fill: "var(--color-alquileres)" },
+  { month: "Total Multas", valor: 62550000.0, fill: "var(--color-multas)" },
+  { month: "Total Ingresos No Tributarios", valor: 1581445400.0, fill: "var(--color-ingresosnotributarios)" },
+  { month: "Total Intereses por prestamos", valor: 2000000.0, fill: "var(--color-tasasinteresesporprestamos)" },
+  { month: "Total De administracion nacional", valor: 494741419.7, fill: "var(--color-totaladmnacional)" },
   {
     month: "Total De gobiernos e institucional provinciales y municipales",
-    totalgipm: 762787123.47,
+    valor: 762787123.47,
+    fill: "var(--color-totalgipm)"
   },
   {
     month: "Total Transferencias corrientes",
-    transferenciacorriente: 1257528543.17,
+    valor: 1257528543.17,
   },
-  { month: "Total Venta de activos", ventaactivos: 240000000.0 },
-  { month: "Total Contribucion por mejoras", contribucionmejoras: 20351000.0 },
+  { month: "Total Venta de activos", valor: 240000000.0, fill: "var(--color-totalventasactivos)" },
+  { month: "Total Contribucion por mejoras", valor: 20351000.0, fill: "var(--color-totalcontribucionmejoras)" },
   {
     month: "Total Recursos propios de capital",
-    recursospropiosdecapital: 260351000.0,
+    valor: 260351000.0,
+    fill: "var(--color-totalrecursospropiosdecapital)"
   },
-  { month: "Total Del sector privado", totalsectorprivado: 21490000.0 },
+  { month: "Total Del sector privado", valor: 21490000.0, fill: "var(--color-totalprivados)" },
 ]
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "#2563eb",
+  totalcontribucionmejoras: {
+    label: "Total Del sector privado",
+    color: "#fc3a10 ",
   },
-  mobile: {
-    label: "Mobile",
-    color: "#60a5fa",
+  totalventasactivos: {
+    label: "totalrecursospropiosdecapital",
+    color: "#ecff00"
   },
+  coparticipacionimpuestos: {
+    label: "Total Coparticipacion de impuestos",
+    color: " #2ae3db"
+  }
 } satisfies ChartConfig
 
 export function GraficosIngresos ({ chart, setChart }) {
   return (
     <div className="flex justify-center items-center">
-      {!chart
+      {chart
         ? (
         <ChartContainer
           config={chartConfig}
-          className="min-h-[200px] w-[850px] "
+          className="min-h-[200px] w-[100%] "
         >
           <BarChart accessibilityLayer data={chartData}>
             <CartesianGrid vertical={false} />
@@ -79,75 +89,40 @@ export function GraficosIngresos ({ chart, setChart }) {
             />
             <ChartTooltip content={<ChartTooltipContent />} />
             <ChartLegend content={<ChartLegendContent />} />
-            <Bar
-              dataKey="coparticipacionimpuestos"
-              fill="var(--color-mobile)"
-              radius={4}
-            />
-            <Bar dataKey="tasas" fill="var(--color-desktop)" radius={4} />
-            <Bar dataKey="derechos" fill="var(--color-desktop)" radius={4} />
-            <Bar dataKey="alquileres" fill="var(--color-desktop)" radius={4} />
-            <Bar dataKey="multas" fill="var(--color-desktop)" radius={4} />
-            <Bar
-              dataKey="notributarios"
-              fill="var(--color-desktop)"
-              radius={4}
-            />
-            <Bar
-              dataKey="interesprestamos"
-              fill="var(--color-desktop)"
-              radius={4}
-            />
-            <Bar dataKey="admnacional" fill="var(--color-desktop)" radius={4} />
-            <Bar dataKey="totalgipm" fill="var(--color-desktop)" radius={4} />
-            <Bar
-              dataKey="transferenciacorriente"
-              fill="var(--color-desktop)"
-              radius={4}
-            />
-            <Bar
-              dataKey="ventaactivos"
-              fill="var(--color-desktop)"
-              radius={4}
-            />
-            <Bar
-              dataKey="contribucionmejoras"
-              fill="var(--color-desktop)"
-              radius={4}
-            />
-            <Bar
-              dataKey="recursospropiosdecapital"
-              fill="var(--color-desktop)"
-              radius={4}
-            />
-            <Bar
-              dataKey="totalsectorprivado"
-              fill="var(--color-desktop)"
-              radius={4}
-            />
+
+            <Bar dataKey="valor" fill="var(--color-desktop)" radius={4} />
           </BarChart>
         </ChartContainer>
           )
         : (
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
-        >
-          <PieChart>
-            <ChartTooltip content={<ChartTooltipContent />} />
-            <Pie data={chartData} dataKey="month">
-              <LabelList
-                dataKey="month"
-                className="fill-background"
-                stroke="none"
-                fontSize={12}
-                formatter={(value: keyof typeof chartConfig) =>
-                  chartConfig[value]?.label
-                }
-              />
-            </Pie>
-          </PieChart>
-        </ChartContainer>
+        <Card className="flex flex-col">
+          <CardHeader className="items-center pb-0">
+            <CardTitle>Grafico Ingresos</CardTitle>
+            <CardDescription>Grafico Ingresos 2023</CardDescription>
+          </CardHeader>
+          <CardContent className="flex-1 pb-0">
+            <ChartContainer
+              config={chartConfig}
+              className="mx-auto aspect-square max-h-[250px]"
+            >
+              <PieChart>
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent />}
+                />
+                <Pie
+                  data={chartData}
+                  dataKey="valor"
+                  nameKey="month"
+                  innerRadius={60}
+                  strokeWidth={5}
+                  fill="var(--color-desktop)"
+                ></Pie>
+              </PieChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col gap-2 text-sm"></CardFooter>
+        </Card>
           )}
     </div>
   )

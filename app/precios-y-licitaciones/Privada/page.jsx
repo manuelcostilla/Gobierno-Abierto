dc8732bfb7e2f0d59227e8eb545ebf0eca7a006d
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-10-22 07:25:06 -0300

    Add BannerText component to private and public tenders pages; update layout styles

--- app/precios-y-licitaciones/Privada/page.jsx
+++ app/precios-y-licitaciones/Privada/page.jsx
@@ -1,12 +1,13 @@
 import { AcordeonDeLicitaciones } from "./components/Licitaciones"
 import { licitaciones } from "./data/InfoLicitaciones"
-
+import { BannerText } from "../../../components/bannertext"
 const Licitacionesprivadas = () => {
   return (
     <>
+    <BannerText titulo='Licitaciones Privadas' />
     <div className='overflow-x-hidden'>
-      <AcordeonDeLicitaciones data={licitaciones}/>
-      </div>
+        <AcordeonDeLicitaciones data={licitaciones}/>
+    </div>
     </>
   )
 }

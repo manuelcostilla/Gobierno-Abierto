647274af11862538e1fa4f095e60598ae0c44efc
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-10-30 12:51:32 -0300

    data de esquema

---
+++ app/organigrama/data/esquema.js
@@ -0,0 +1,31 @@
+export const Organigrama = [{
+  id: 1,
+  titulo: 'Secretaría de Gobierno',
+  descripcion: 'La Secretaría de Gobierno es el área encargada de coordinar y supervisar las políticas públicas del municipio. Además, se encarga de la administración de los recursos humanos y materiales del municipio.',
+  img: '/images/organigrama/secretaria_gobierno.jpg',
+},
+{
+  id: 2,
+  titulo: 'Secretaría de Hacienda',
+  descripcion: 'La Secretaría de Hacienda es el área encargada de la administración de los recursos económicos del municipio. Además, se encarga de la recaudación de impuestos y tasas municipales.',
+  img: '/images/organigrama/secretaria_hacienda.jpg'
+},
+{
+  id: 3,
+  titulo: 'Secretaría de Obras Públicas',
+  descripcion: 'La Secretaría de Obras Públicas es el área encargada de la planificación y ejecución de las obras públicas del municipio. Además, se encarga de la conservación y mantenimiento de la infraestructura urbana.',
+  img: '/images/organigrama/secretaria_obras_publicas.jpg'
+},
+{
+  id: 4,
+  titulo: 'Secretaría de Desarrollo Social',
+  descripcion: 'La Secretaría de Desarrollo Social es el área encargada de la implementación de políticas sociales del municipio. Además, se encarga de la promoción y protección de los derechos de los ciudadanos.',
+  img: '/images/organigrama/secretaria_desarrollo_social.jpg'
+},
+{
+  id: 5,
+  titulo: 'Secretaría de Salud',
+  descripcion: 'La Secretaría de Salud es el área encargada de la promoción y protección de la salud de los ciudadanos. Además, se encarga de la prevención y control de enfermedades.',
+  img: '/images/organigrama/secretaria_salud.jpg'
+},
+]

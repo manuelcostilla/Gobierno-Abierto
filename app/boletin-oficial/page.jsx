cc7935827c176f262448f475e89a05ee4f21ee24
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-11-04 09:16:54 -0300

    prueba

--- app/boletin-oficial/page.jsx
+++ app/boletin-oficial/page.jsx
@@ -1,15 +1,38 @@
 import BoletinOficialButtons from "./components/BoletinOficialButtons"
 import { BannerText } from "../../components/bannertext"
 
+const contenidoItems = [
+  {
+    id: 1,
+    text: "Ordenanzas Municipales",
+  },
+  {
+    id: 2,
+    text: "Decretos del Honorable Concejo Deliberante (HCD)",
+  },
+  {
+    id: 3,
+    text: "Resoluciones del HCD",
+  },
+  {
+    id: 4,
+    text: "Decretos del Departamento Ejecutivo",
+  },
+  {
+    id: 5,
+    text: "Resoluciones del Departamento Ejecutivo",
+  },
+]
+
 const PdfDownloader = () => {
   return (
     <>
-      <div className="overflow-x-hidden mx-auto ">
-        <BannerText titulo="Boletín Oficial" />
-        <div className="grid grid-cols-5 grid-rows-1 pt-10 pb-10">
-          <div className="col-start-3">
+      <BannerText titulo="Boletín Oficial" />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10 mb-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+          <div>
             <p className="text-xl font-bold">¿Qué es el Boletín Oficial?</p>
-            <p className=" text-balance">
+            <p className="text-balance">
               El Boletín Oficial es un medio de comunicación institucional que
               tiene como objetivo informar a la ciudadanía sobre los actos
               administrativos, resoluciones y avisos de interés público. A
@@ -17,39 +40,22 @@ const PdfDownloader = () => {
               a la información pública.
             </p>
           </div>
-          <div className="col-start-4">
-            <p className="text-xl font-bold">Contenido del Boletín</p>
-            <p>En nuestro Boletín Oficial podrás encontrar:</p>
-            <ul>
-              <li className="text-balance">
-                Ordenanzas Municipales: Legislación local aprobada que regula
-                diversas áreas de la vida municipal.
-              </li>
-
-              <li className="text-balance">
-                Decretos del Honorable Concejo Deliberante (HCD): Resoluciones y
-                decretos emitidos por el HCD, que abordan asuntos de interés
-                público y normativas locales.
-              </li>
-              <li className="text-balance">
-                Resoluciones del HCD: Decisiones tomadas por el HCD en relación
-                con diversas materias que afectan a la comunidad.
-              </li>
-              <li className="text-balance">
-                Decretos del Departamento Ejecutivo: Actos administrativos
-                emitidos por el Alcalde y su equipo, relacionados con la gestión
-                y administración del municipio.
-              </li>
-              <li className="text-balance">
-                Resoluciones del Departamento Ejecutivo: Determinaciones y
-                decisiones operativas del Departamento Ejecutivo para la
-                implementación de políticas públicas.
-              </li>
-            </ul>
+          <div>
+            <p className="text-xl font-bold mb-4">Contenido del Boletín</p>
+            <p className="mb-4">En nuestro Boletín Oficial podrás encontrar:</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <ol className="space-y-4 ">
+                {contenidoItems.map((item) => (
+                  <li key={item.id} className="text-balance">
+                    {item.text}
+                  </li>
+                ))}
+              </ol>
+            </div>
           </div>
         </div>
-        <BoletinOficialButtons />
       </div>
+      <BoletinOficialButtons />
     </>
   )
 }

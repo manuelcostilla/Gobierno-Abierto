02e2558617bbb16d7bcff8a48210d5366ab8a9e8
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-11-05 10:02:17 -0300

    abc

--- app/boletin-oficial/page.jsx
+++ app/boletin-oficial/page.jsx
@@ -1,57 +1,21 @@
 import BoletinOficialButtons from "./components/BoletinOficialButtons"
 import { BannerText } from "../../components/bannertext"
 
-const contenidoItems = [
-  {
-    id: 1,
-    text: "Ordenanzas Municipales",
-  },
-  {
-    id: 2,
-    text: "Decretos del Honorable Concejo Deliberante (HCD)",
-  },
-  {
-    id: 3,
-    text: "Resoluciones del HCD",
-  },
-  {
-    id: 4,
-    text: "Decretos del Departamento Ejecutivo",
-  },
-  {
-    id: 5,
-    text: "Resoluciones del Departamento Ejecutivo",
-  },
-]
-
 const PdfDownloader = () => {
   return (
     <>
       <BannerText titulo="Boletín Oficial" />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10 mb-10">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+      <div className=" container mx-auto">
+        <div className="flex justify-center items-center">
           <div>
-            <p className="text-xl font-bold">¿Qué es el Boletín Oficial?</p>
-            <p className="text-balance">
+            <p className="text-xl font-bold ">¿Qué es el Boletín Oficial?</p>
+            <p className="text-balance ">
               El Boletín Oficial es un medio de comunicación institucional que
               tiene como objetivo informar a la ciudadanía sobre los actos
               administrativos, resoluciones y avisos de interés público. A
               través de este boletín, garantizamos la transparencia y el acceso
               a la información pública.
             </p>
-          </div>
-          <div>
-            <p className="text-xl font-bold mb-4">Contenido del Boletín</p>
-            <p className="mb-4">En nuestro Boletín Oficial podrás encontrar:</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <ol className="space-y-4 ">
-                {contenidoItems.map((item) => (
-                  <li key={item.id} className="text-balance">
-                    {item.text}
-                  </li>
-                ))}
-              </ol>
-            </div>
           </div>
         </div>
       </div>
--- app/presupuesto-publico/2020/ingresos-2020/page.jsx
+++ app/presupuesto-publico/2020/ingresos-2020/page.jsx
@@ -5,7 +5,7 @@
 // import { Button } from "@/components/ui/button"
 
 import { GraficosIngresos2020 } from "./components/grafico_ingresos_2020"
-import { GraficosIngresosProcedencia2020 } from "./components/grafico_ingresos_procedencia_2020"
+// import { GraficosIngresosProcedencia2020 } from "./components/grafico_ingresos_procedencia_2020"
 import { BannerText } from "@/components/bannertext"
 import { CardContentPp } from "@/components/cardtemplate"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
@@ -14,53 +14,42 @@ export default function Ingresos2020 () {
   return (
     <>
       <BannerText titulo="Ingresos" />
-      <div className="conntainer mx-auto my-10 px-20">
-        <div className="grid grid-cols-1  lg:grid-cols-5 lg:grid-rows-1 lg:gap-12">
-          <div className="col-span-2 lg:col-span-2 lg:col-start-1">
-            <Card>
-              <CardHeader>
-                <CardTitle>Grafico Ingresos por rubro en pesos 2020</CardTitle>
-              </CardHeader>
-              <CardContent className="flex justify-center items-center">
-                <CardContentPp title="Total" content="$45.223.445" />
-                <CardContentPp
-                  title="Diferencia 2020-2019"
-                  content="$35.000.000"
-                />
-                <CardContentPp title="Aumento porcentual" content="25%" />
-                <CardContentPp title="Impuestos recaudados" content="Abl" />
-              </CardContent>
-              <CardContent>
-                <Card>
-                  <CardContent>
-                    <GraficosIngresos2020 />
-                  </CardContent>
-                </Card>
-              </CardContent>
-            </Card>
-          </div>
-
-          <div className="col-span-2 lg:col-span-2 lg:col-start-4">
-            <Card>
-              <CardHeader>
-                <CardTitle>Grafico Ingresos por rubro en pesos 2020</CardTitle>
-              </CardHeader>
-              <CardContent className="flex justify-center items-center   ">
-                <CardContentPp title="Total" content="$45.223.445" />
-                <CardContentPp
-                  title="Diferencia 2020-2019"
-                  content="$35.000.000"
-                />
-                <CardContentPp title="Aumento porcentual" content="25%" />
-                <CardContentPp title="Impuesto recaudados" content="Abl" />
-              </CardContent>
+      <div className="conntainer mx-auto my-10 px-20 flex justify-center items-center">
+        <div className="flex justify-center w-full">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-2xl">
+                Grafico Ingresos por rubro en pesos 2020
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex justify-center items-center">
+              <CardContentPp title="Total" content="$45.223.445" />
+              <CardContentPp
+                title="Diferencia 2020-2019"
+                content="$35.000.000"
+              />
+              <CardContentPp title="Aumento porcentual" content="25%" />
+              <CardContentPp title="Impuestos recaudados" content="Abl" />
+            </CardContent>
+            <CardContent>
               <Card>
                 <CardContent>
-                  <GraficosIngresosProcedencia2020 />
+                  <GraficosIngresos2020 />
                 </CardContent>
               </Card>
-            </Card>
-          </div>
+            </CardContent>
+            <CardContent>
+              <Card>
+                <CardContent className="flex justify-end">
+                  <div>
+                    <p>
+                      llfjfjjjkjklkskjkfkjfksjkljkskjskjsksakjsfjlksfjklfsjklsfkljsjklsfklj
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </>

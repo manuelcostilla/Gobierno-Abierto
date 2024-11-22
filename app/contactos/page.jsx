795a2405fcfdc3c365d9f89913cb58bca2e892a3
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2024-11-22 09:18:44 -0300

    responsive contactos

--- app/contactos/page.jsx
+++ app/contactos/page.jsx
@@ -11,27 +11,37 @@ function Contactos () {
           title="bienvenido"
           text="Nos importa tu opinión y estamos aquí para ayudarte. Si tienes consultas, sugerencias o necesitas información sobre los servicios municipales, no dudes en ponerte en contacto con nosotros."
         />
-        <div className="flex justify-center items-center pt-10 pb-10">
-          <SectionCard
-            titulo="Baradero"
-            link="/contactos/baradero"
-            textlink="Ver más información"
-          />
-          <SectionCard
-            titulo="Portela"
-            link="/contactos/portela"
-            textlink="Ver más información"
-          />
-          <SectionCard
-            titulo="Santa Coloma"
-            link="/contactos/santa-coloma"
-            textlink="Ver más información"
-          />
-          <SectionCard
-            titulo="Alsina"
-            link="/contactos/alsina"
-            textlink="Ver más información"
-          />
+        <div className="flex flex-wrap justify-center items-center pt-20 m-5">
+          <div className="m-5">
+            <SectionCard
+              titulo="Baradero"
+              link="/contactos/baradero"
+              textlink="Ver más información"
+            />
+          </div>
+
+          <div className="m-5">
+            <SectionCard
+              titulo="Portela"
+              link="/contactos/portela"
+              textlink="Ver más información"
+            />
+          </div>
+
+          <div className="m-5">
+            <SectionCard
+              titulo="Santa Coloma"
+              link="/contactos/santa-coloma"
+              textlink="Ver más información"
+            />
+          </div>
+          <div className="m-5">
+            <SectionCard
+              titulo="Alsina"
+              link="/contactos/alsina"
+              textlink="Ver más información"
+            />
+          </div>
         </div>
       </div>
     </>

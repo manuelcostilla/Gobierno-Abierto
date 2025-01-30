892cb38b2f7fba2f09046936ddc1879b6eaf6fa7
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2025-01-30 11:11:40 -0300

    areas de gobierno actualizacion

--- app/testpage/page.jsx
+++ app/testpage/page.jsx
@@ -5,17 +5,17 @@ import { BannerText } from "@/components/bannertext"
 function testPage () {
   return (
     <>
-    <BannerText titulo="Áreas de Gobierno"/>
-      <div className="mx-auto container pt-20 pb-20">
-        <section className="grid grid-cols-1 gap-24 md:gird-cols-2 lg:grid-cols-3  bg-gray-50 border rounded-md p-20">
+      <BannerText titulo="Áreas de Gobierno" />
+      <div className="mx-auto container pt-20 pb-20 flex justify-center items-center">
+        <section className="grid grid-cols-1 gap-10 w-[1050px] md:gird-cols-2 lg:grid-cols-3  bg-gray-50 border rounded-md p-20">
           {secretarias.map((secretarias) => {
             return (
               <article key={secretarias.id}>
-              <Link href={`/testpage/${secretarias.id}`}>
-                <h2 className="flex gap-10 font-bold text-xl text-slate-700 text-left text-balance">
-                  <span>{secretarias.icon}</span>
-                  <span>{secretarias.nombre}</span>
-                </h2>
+                <Link href={`/testpage/${secretarias.id}`}>
+                  <h2 className="flex font-semibold text-base m-5  text-slate-700 text-balance">
+                    <span className="m-2">{secretarias.icon}</span>
+                    <span>{secretarias.nombre}</span>
+                  </h2>
                 </Link>
               </article>
             )

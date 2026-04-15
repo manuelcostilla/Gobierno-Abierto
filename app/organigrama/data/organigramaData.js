245c390efb4d5d5e61ce6817891f14d12c947f92
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2026-04-15 10:57:33 -0300

    Modernización del portal: Header, Banner, Atajos, Footer y Mapa del Estado Municipal interactivo

---
+++ app/organigrama/data/organigramaData.js
@@ -0,0 +1,78 @@
+import { 
+  Network
+} from "lucide-react"
+
+export const organigramaData = [
+  {
+    id: "item-1",
+    sector: "INTENDENCIA",
+    responsable: "ESTEBAN DAMIÁN SANZIO",
+    cargo: "Intendente",
+    icon: Network,
+    img: "/imgorganigrama/CONDUCCIONSUPERIOR.jpg",
+    pdf: "/pdf/CONDUCCIONSUPERIOR.pdf"
+  },
+  {
+    id: "item-2",
+    sector: "SECRETARÍA GENERAL DE GOBIERNO",
+    responsable: "LUCIANA DE PAULI",
+    cargo: "Secretaria",
+    icon: Network,
+    img: "/imgorganigrama/SECRETARIAGOBIERNO.jpg",
+    pdf: "/pdf/SECRETARIADEGOBIERNO.pdf"
+  },
+  {
+    id: "item-3",
+    sector: "SECRETARÍA DE PLANIFICACIÓN Y GESTIÓN PÚBLICA",
+    responsable: "ELIANA GENTILE",
+    cargo: "Secretaria",
+    icon: Network,
+    img: "/imgorganigrama/SECRETARIAPLANIFICACION.jpg",
+    pdf: "/pdf/SECRETARIADEPLANIFICACIONYGESTIONPUBLICA.pdf"
+  },
+  {
+    id: "item-4",
+    sector: "SECRETARÍA DE OBRAS Y SERVICIOS PÚBLICOS",
+    responsable: "ERIKA CARLSSON",
+    cargo: "Secretaria",
+    icon: Network,
+    img: "/imgorganigrama/SECRETARIAOBRASSERVICIOS.jpg",
+    pdf: "/pdf/SECRETARIADEOBRASYSERVICIOSPUBLICOS.pdf"
+  },
+  {
+    id: "item-5",
+    sector: "SECRETARÍA DE HACIENDA Y FINANZAS PÚBLICAS",
+    responsable: "FABIO ARTIMIAK",
+    cargo: "Secretario",
+    icon: Network,
+    img: "/imgorganigrama/SECRETARIAHACIENDA.jpg",
+    pdf: "/pdf/SECRETARIADEHACIENDAYFINANZASPUBLICAS.pdf"
+  },
+  {
+    id: "item-6",
+    sector: "SECRETARÍA DE CULTURA, EDUCACIÓN, COMUNICACIÓN Y DEPORTES",
+    responsable: "SANTIAGO BALDI",
+    cargo: "Secretario",
+    icon: Network,
+    img: "/imgorganigrama/SECRETARIACULTURA.jpg",
+    pdf: "/pdf/SECRETARIADECULTURAEDUCACIONCOMUNICACIONYDEPORTES.pdf"
+  },
+  {
+    id: "item-7",
+    sector: "SECRETARÍA DE SALUD",
+    responsable: "DARIO JOSÉ PUEDE",
+    cargo: "Secretario",
+    icon: Network,
+    img: "/imgorganigrama/SECRETARIASALUD.jpg",
+    pdf: "/pdf/SECRETARIADESALUD.pdf"
+  },
+  {
+    id: "item-8",
+    sector: "SECRETARÍA DE DESARROLLO SOCIAL",
+    responsable: "SANTIAGO RIOS",
+    cargo: "Secretario",
+    icon: Network,
+    img: "/imgorganigrama/SECRETARIADESOCIAL.jpg",
+    pdf: "/pdf/SECRETARIADEDESARROLLOSOCIAL.pdf"
+  }
+]

7a69f6356bcae3a4f30567b11c58a043aaf56a22
Manuel Costilla <santiagomanueldariocostilla@gmail.com>
2026-07-08 08:57:51 -0300

    ocultado direcciones a mapa del estado y gest de compromisos

--- components/dataAtajos.jsx
+++ components/dataAtajos.jsx
@@ -1,14 +1,14 @@
 
 
 export const dataAtajos = [
-  {
-    id: 1,
-    title: 'MAPA DEL ESTADO MUNICIPAL',
-    img: 'mapa_estado_municipal',
-    alt: 'Mapa del estado municipal',
-    descripcion: 'Es un diagrama que muestra la estructura jerárquica funcional del gobierno local, indicando las áreas y sus relaciones. Es un aporte a la transparencia y un acercamiento a la ciudadanía a un gobierno abierto ya que permite conocer la estructura organizativa.',
-    link: '../organigrama',
-  },
+  // {
+  //   id: 1,
+  //   title: 'MAPA DEL ESTADO MUNICIPAL',
+  //   img: 'mapa_estado_municipal',
+  //   alt: 'Mapa del estado municipal',
+  //   descripcion: 'Es un diagrama que muestra la estructura jerárquica funcional del gobierno local, indicando las áreas y sus relaciones. Es un aporte a la transparencia y un acercamiento a la ciudadanía a un gobierno abierto ya que permite conocer la estructura organizativa.',
+  //   link: '../organigrama',
+  // },
   {
     id: 2,
     title: 'BOLETÍN OFICIAL',
@@ -25,14 +25,14 @@ export const dataAtajos = [
     descripcion: 'Aquí encontrarás detalles sobre las licitaciones y contratos del municipio. Incluye anuncios de licitaciones, contratos adjudicados, y detalles sobre proveedores y gastos. Esta sección asegura la transparencia al mostrar cómo se adjudican los contratos y se manejan las compras públicas.',
     link: '../precios-y-licitaciones',
   },
-  {
-    id: 4,
-    title: 'GESTIÓN DE COMPROMISOS',
-    img: 'gestion_compromisos',
-    alt: 'Gestión de compromisos',
-    descripcion: 'En esta sección, se detallan los compromisos del municipio relacionados con obras públicas, incluyendo proyectos en curso y futuros. Los ciudadanos pueden seguir el avance de las obras, conocer los plazos y presupuestos, y verificar el cumplimiento de los compromisos, promoviendo la transparencia y el control sobre el uso de los recursos públicos.',
-    link: '../Gestion-de-compromisos',
-  },
+  // {
+  //   id: 4,
+  //   title: 'GESTIÓN DE COMPROMISOS',
+  //   img: 'gestion_compromisos',
+  //   alt: 'Gestión de compromisos',
+  //   descripcion: 'En esta sección, se detallan los compromisos del municipio relacionados con obras públicas, incluyendo proyectos en curso y futuros. Los ciudadanos pueden seguir el avance de las obras, conocer los plazos y presupuestos, y verificar el cumplimiento de los compromisos, promoviendo la transparencia y el control sobre el uso de los recursos públicos.',
+  //   link: '../Gestion-de-compromisos',
+  // },
   {
     id: 5,
     title: 'PRESUPUESTO PÚBLICO',
